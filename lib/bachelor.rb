efc724453d10d2e7c6291f3361c2daf7b57ab89f
Samidh Patel <spatel13456@gmail.com>
2019-01-17 21:02:41 +0000

    Done.

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,19 +1,63 @@
 def get_first_name_of_season_winner(data, season)
-  # code here
+  winner = nil
+  data.each do |season_number, detail|
+    if season_number == season
+    detail.each do |key, value|
+      if key["status"] == "Winner"
+        winner = key["name"]
+        end
+      end
+    end
+  end
+  winner.split(" ").first
 end
 
 def get_contestant_name(data, occupation)
-  # code here
+  name = nil
+  data.each do |season_number, detail|
+    detail.each do |key, value|
+      if key["occupation"] == occupation
+        name = key["name"]
+      end
+    end
+  end
+  return name
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+  count = 0
+  data.each do |season_number, detail|
+    detail.each do |key, value|
+      if key["hometown"] == hometown
+        count += 1
+      end
+    end
+  end
+  return count
 end
 
 def get_occupation(data, hometown)
-  # code here
+  name = nil
+  data.each do |season_number, detail|
+    detail.each do |key, value|
+      if key["hometown"] == hometown && name == nil
+        name = key["occupation"]
+      end
+    end
+  end
+  return name
 end
 
 def get_average_age_for_season(data, season)
-  # code here
+  average_age = []
+  data.each do |season_number, detail|
+    if season_number == season
+      detail.each do |key, value|
+        average_age << key["age"]
+      end
+    end
+  end
+  num = 0
+  average_age.each {|age| num += age.to_f}
+  average_age = (num/average_age.length).round
 end
