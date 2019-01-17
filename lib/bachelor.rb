dd66e10ad344b776f8bb37528afd0b41a616d8cd
Samidh Patel <spatel13456@gmail.com>
2019-01-17 20:09:01 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,52 +1,17 @@
 def get_first_name_of_season_winner(data, season)
-  winner = ""
-  data.each do |season_number, detail|
-    if season == season_number
-      detail.each do |key, value|
-        if key["status"] == "Winner"
-          winner = key["winner"]
-        end
-      end
-    end
-  end
-  new_array = []
-  new_array = winner.split(" ")
-  return new_array[0]
+  
 end
 
 def get_contestant_name(data, occupation)
-  name = ""
-  data.each do |season_number, detail|
-    detail.each do |key, value|
-      if key["occupation"] == occupation
-        return key["name"]
-      end
-    end
-  end
+  
 end
 
 def count_contestants_by_hometown(data, hometown)
-  counter = 0
-  data.each do |season_number, detail|
-    detail.each do |key, value|
-      if key["hometown"] == hometown
-        counter += 1
-      end
-    end
-  end
-  return counter
+  
 end
 
 def get_occupation(data, hometown)
-  hometown = ""
-  data.each do |season_number, detail|
-    detail.each do |key, value|
-      if key["hometown"] == hometown
-        return key["occupation"]
-        break
-      end
-    end
-  end
+  
 end
 
 def get_average_age_for_season(data, season)
