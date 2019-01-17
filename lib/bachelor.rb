c4089f21728520cb1b2cc840d147fc93cbede664
Samidh Patel <spatel13456@gmail.com>
2019-01-17 20:29:36 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -25,7 +25,15 @@ def get_contestant_name(data, occupation)
 end
 
 def count_contestants_by_hometown(data, hometown)
-  
+  count = 0
+  data.each do |season_number, detail|
+    detail.each do |key, value|
+      if key["hometown"] == hometown
+        counter += 1
+      end
+    end
+  end
+  return count
 end
 
 def get_occupation(data, hometown)
